143cc31e8ff40669d3f7793617d6c545d39d4387
Phuong Phung <phuongthephung@gmail.com>
2023-06-24 02:35:11 +0700

    feat: add gtag.js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 interface SEOMeta {
   name?: string
@@ -77,6 +78,24 @@ class MyDocument extends Document {
           {meta.map(({ name, content, property, key }) => (
             <meta key={key} name={name} property={property} content={content} />
           ))}
+
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WQ5GTT6');`}
+          </Script>
+
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4R32XCF1J4"></Script>
+          <Script id="google-analytic-manager" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4R32XCF1J4');
+`}
+          </Script>
         </Head>
 
         <body>
