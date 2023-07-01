03fa5bad6ca30a2d125d6fb450c6344c180ce374
Phuong Phung <phuongthephung@gmail.com>
2023-07-02 00:24:22 +0700

    fix: use absolute path for fs get posts

--- src/utils/content-helper.ts
+++ src/utils/content-helper.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export const getAllPosts = (): string[] => {
-  const files = fs.readdirSync('contents')
+  const files = fs.readdirSync(`${process.cwd()}/contents`)
   const paths = files.map((fileName) => fileName.replace('.mdx', ''))
   return paths
 }
