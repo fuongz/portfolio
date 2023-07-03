f9aedfd989a9832cfe9aadbcf03b10ecd0fa4b2c
Phuong Phung <phuongthephung@gmail.com>
2023-07-03 10:53:42 +0700

    feat: add next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+const config = {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: process.env.NEXT_PUBLIC_ROOT_URL,
+    siteName: 'SiteName',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
+
+export default config
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,73 +6,10 @@ import NextNProgress from 'nextjs-progressbar'
 import Header from '@/components/Shared/Header'
 import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
-
-interface SEOMeta {
-  name?: string
-  content: string
-  property?: string
-  key?: string
-}
+import { DefaultSeo } from 'next-seo'
+import SEOConfig from '../../next-seo.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const meta: SEOMeta[] = [
-    {
-      name: 'description',
-      content: "Phuong Phung's portfolio.",
-      key: 'description',
-    },
-    {
-      property: 'og:description',
-      content: "Phuong Phung's portfolio.",
-      key: 'og:description',
-    },
-    {
-      property: 'og:type',
-      content: 'website',
-      key: 'og:type',
-    },
-    {
-      property: 'og:image',
-      content: process.env.NEXT_PUBLIC_ROOT_URL + '/og-default.png',
-      key: 'og:image',
-    },
-    {
-      property: 'og:image:alt',
-      content: 'Banner for phuongphung.com',
-      key: 'og:image:alt',
-    },
-    {
-      property: 'og:image:width',
-      content: '1280',
-      key: 'og:image:width',
-    },
-    {
-      property: 'og:image:height',
-      content: '675',
-      key: 'og:image:height',
-    },
-    {
-      property: 'twitter:card',
-      content: 'summary_large_image',
-      key: 'twitter:card',
-    },
-    {
-      property: 'twitter:creator',
-      content: '@fuongzt',
-      key: 'twitter:creator',
-    },
-    {
-      property: 'twitter:description',
-      content: "Phuong Phung's portfolio.",
-      key: 'twitter:description',
-    },
-    {
-      property: 'twitter:image',
-      content: process.env.NEXT_PUBLIC_ROOT_URL + '/og-default.png',
-      key: 'twitter:image',
-    },
-  ]
-
   return (
     <>
       <Head>
@@ -80,9 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {meta.map(({ name, content, property, key }) => (
-          <meta key={key} name={name} property={property} content={content} />
-        ))}
+        <DefaultSeo {...SEOConfig} />
       </Head>
       <ThemeProvider enableSystem={true} attribute="class">
         <NextNProgress options={{ easing: 'ease', speed: 500 }} />
