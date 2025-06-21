9937c163db617f99ccac037a05e38c775e060baa
fuongz <hi@phuongphung.com>
2025-06-21 16:14:10 +0700

    ✨ feat: gh graph contribution chart with dark mode

--- src/components/Shared/contribute-graph.tsx
+++ src/components/Shared/contribute-graph.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import type { TContributeGraph } from "types/contribute-graph";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui";
 
 function formatDate(dateString: string): string {
 	const date = new Date(dateString);
@@ -13,6 +14,16 @@ function formatDate(dateString: string): string {
 	});
 }
 
+const COLORS: {
+	[key: string]: string;
+} = {
+	"#ebedf0": "bg-zinc-100 dark:bg-zinc-900",
+	"#9be9a8": "bg-green-200 dark:bg-green-950",
+	"#30a14e": "bg-green-300 dark:bg-green-800",
+	"#40c463": "bg-green-400 dark:bg-green-600",
+	"#216e39": "bg-green-500 dark:bg-green-400",
+};
+
 export function ContributeGraph({ weeks }: { weeks: TContributeGraph }) {
 	const totalContributions = weeks.reduce((total, week) => {
 		return (
@@ -22,12 +33,36 @@ export function ContributeGraph({ weeks }: { weeks: TContributeGraph }) {
 			}, 0)
 		);
 	}, 0);
+	const totalDays = weeks.reduce((total, week) => {
+		return total + week.contributionDays.length;
+	}, 0);
 
 	return (
 		<>
-			<div className="mb-4 text-sm text-zinc-400 ml-auto">
-				<b>{totalContributions}</b> contributions in the last <b>{35 * 7}</b>{" "}
-				days
+			<div className="flex mb-4 justify-between">
+				<div className="text-sm text-zinc-400">
+					<span className="text-bold dark:text-white text-zinc-900">
+						{totalContributions}
+					</span>{" "}
+					contributions in the last{" "}
+					<span className="text-bold dark:text-white text-zinc-900">
+						{totalDays}
+					</span>{" "}
+					days
+				</div>
+
+				<div className="flex gap-2 items-center text-xs text-zinc-400">
+					<span>Less</span>
+					<div className="flex gap-0.5">
+						{Object.keys(COLORS).map((color, index) => (
+							<div
+								key={`color-${index}`}
+								className={`w-3 h-3 rounded-sm cursor-pointer transition-all ${COLORS[color]}`}
+							></div>
+						))}
+					</div>
+					<span>More</span>
+				</div>
 			</div>
 			<div className="space-y-4 w-full">
 				{/* Contribution grid */}
@@ -35,18 +70,23 @@ export function ContributeGraph({ weeks }: { weeks: TContributeGraph }) {
 					{weeks.map((week, weekIndex) => (
 						<div key={`week-${weekIndex}`} className="flex flex-col gap-1">
 							{week.contributionDays.map((day, dayIndex) => (
-								<div
-									key={`day-${weekIndex}-${dayIndex}`}
-									className={`w-3 h-3 rounded-sm cursor-pointer transition-all hover:ring-2 hover:ring-gray-400`}
-									style={{
-										backgroundColor: day.color,
-									}}
-									title={
-										day.date
+								<Tooltip key={`day-${weekIndex}-${dayIndex}`}>
+									<TooltipTrigger asChild>
+										<div
+											className={`w-3 h-3 rounded-sm cursor-pointer transition-all hover:ring-2 hover:ring-gray-400 ${COLORS[day.color]}`}
+											title={
+												day.date
+													? `${day.contributionCount} contributions on ${formatDate(day.date)}`
+													: ""
+											}
+										/>
+									</TooltipTrigger>
+									<TooltipContent>
+										{day.date
 											? `${day.contributionCount} contributions on ${formatDate(day.date)}`
-											: ""
-									}
-								/>
+											: ""}
+									</TooltipContent>
+								</Tooltip>
 							))}
 						</div>
 					))}
